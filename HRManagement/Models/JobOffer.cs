29a1ba5424faab70a2dee3052a2b93a781be2de3
martalech <martalech98@gmail.com>
2019-11-17 16:51:06 +0100

    Initialize job offers page

---
+++ HRManagement/Models/JobOffer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRManagement.Models
+{
+    public class JobOffer
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public int Salary { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string ContractType { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
