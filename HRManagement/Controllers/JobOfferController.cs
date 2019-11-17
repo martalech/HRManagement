1e12e1dd37bc44d9508c227d1b2cc3c3386b7801
martalech <martalech98@gmail.com>
2019-11-17 16:51:06 +0100

    Initialize job offers page

---
+++ HRManagement/Controllers/JobOfferController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRManagement.Controllers
+{
+    [Route("[controller]")]
+    public class JobOfferController : Controller
+    {
+        private static List<JobOffer> _jobOffers = new List<JobOffer>
+        {
+            new JobOffer {Id=1, JobTitle="Backend Developer"},
+            new JobOffer {Id=2, JobTitle="Frontend Developer"},
+            new JobOffer {Id=3, JobTitle="Manager"},
+            new JobOffer {Id=4, JobTitle="Teacher"},
+            new JobOffer {Id=5, JobTitle="Cook"}
+        };
+
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View(_jobOffers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_jobOffers.FirstOrDefault(o => o.Id == id));
+        }
+    }
+}
