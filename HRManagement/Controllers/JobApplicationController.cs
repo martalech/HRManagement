80b5195035a721fdd55db76f1bb6825e1697b03f
martalech <martalech98@gmail.com>
2019-11-23 13:22:13 +0100

    Add job application page

---
+++ HRManagement/Controllers/JobApplicationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HRManagement.Models;
+
+namespace HRManagement.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class JobApplicationController : Controller
+    {
+        public IActionResult ApplyFor(int offerId)
+        {
+            var jobApp = new JobApplication();
+            jobApp.OfferId = offerId;
+            return View(jobApp);
+        }
+    }
+}
